bd4e561278632626b7d9a6b7368519e75e212a90
aissy <aissypewdiepie@gmail.com>
2020-12-25 22:15:12 +0530

    just barely works

--- src/wrapper1.cpp
+++ src/wrapper1.cpp
@@ -1,8 +1,8 @@
 #include </home/aissy/Documents/currun/src/arith_uint256.h>
+#include </home/aissy/Documents/currun/src/uint256.h>
 
 void uint_error(std::string& str);
 template <unsigned int BITS>
-
 base_uint<BITS>::base_uint(const std::string& str) {
     BITS/2 ;
     base_uint<BITS> a;
@@ -15,10 +15,29 @@ void test(const char* args, const int int_arg) {
     std::string& y = (std::string&)args;
     const int s = int_arg + 1 - 1;
     const int s_ = 32;
-    // s is not const for some reason
+    // s is not const for some reason : fixed
     base_uint<(const int)s_> uint;
-    uint.k;
+
+    base_uint<s_>::WIDTH;
+    // ERROR : no type defined
+
 }
+
+template <unsigned int BITS>
+double base_uint<BITS>::getdouble() const {
+    //base_uint::WIDTH;
+    base_blob<BITS> a; 
+    a.isNull();
+    getdouble;
+    return 0;
+}
+
+template <unsigned int BITS>
+void base_blob<BITS>::SetHex(const char* x) {}
+
+template <unsigned int BITS>
+base_blob<BITS>::base_blob(const std::vector<unsigned char>& vc) {}
+
 
 int main() {
 // instantnizing this makes problems
